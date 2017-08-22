e6f4c741bec54110d1ba1767cab32e7bce59347a
Your Name <you@example.com>
2017-08-22 07:28:55 -0700

    Working CSS file

--- app.js
+++ app.js
@@ -4,18 +4,34 @@ var path = require("path");
 const app = express();
 const port = 8000;
 
-app.use(express.static("./views/"));
-//homepage
+// serve entire public directory with HTML & CSS files inside
+app.use(express.static(path.resolve(__dirname + "/public")));
+
+// adjust routes with new path to views directory
 app.get("/", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/index.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/index.html"));
 });
+
+/*
+this correction will ensure that express serves the entire public directory (with all of its contents) to the client so any stylesheets, javascripts, and/or images will now be available to the html files
+*/
+
 // about page
 app.get("/about", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/about.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/about.html"));
+});
+
+// background page
+app.get("/background", function(req, res) {
+  res.sendfile(path.resolve(__dirname + "/public/views/background.html"));
+});
+// games page
+app.get("/drones", function(req, res) {
+  res.sendfile(path.resolve(__dirname + "/public/views/drones.html"));
 });
 
 app.get("*", function(req, res) {
-  res.sendfile(path.resolve(__dirname + "/views/404.html"));
+  res.sendfile(path.resolve(__dirname + "/public/views/404.html"));
 });
 app.listen(port, function() {
   console.log("listening on port " + port);
